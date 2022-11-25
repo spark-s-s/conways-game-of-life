efa89ddd5cc161d423537afb51a235ced4f07a76
spark <spark.h.sasaki@gmail.com>
2022-11-25 23:57:41 +0900

    import sleep

--- test/input_and_output_test.py
+++ test/input_and_output_test.py
@@ -1,8 +1,18 @@
 import numpy as np
+from time import sleep 
 
 h = input('H : ')
 w = input('W : ')
 
 field = np.zeros((int(h), int(w)))
 
-print(field)
+while 1:
+    for i in range(int(h)):
+        for j in range(int(w)):
+            if int(field[i][j]) == 0:
+                print('-', end='')
+            else:
+                print('X', end='')
+        print('')
+    print('')
+    sleep(0.5)
